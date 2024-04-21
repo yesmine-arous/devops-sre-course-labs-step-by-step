e9ef5df0334c10a24e85e0f47062f9bca45ac8d6
yesmine-arous <yesmine.arous@craftschoolship.com>
2024-04-22 00:31:05 +0100

    adding step-3.yml

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 """
-
+Simple Flask web application.
 """
 
 from flask import Flask, request, render_template
 
-
-APP = Flask(__name__,template_folder='templates')
-
+APP = Flask(__name__, template_folder='templates')
 
 @APP.route('/')
 def home():
@@ -16,16 +14,15 @@ def home():
     '''
     return render_template('index.html')
 
-
-@APP.route('/hello',methods=['POST'])
+@APP.route('/hello', methods=['POST'])
 def hello():
     '''
     For rendering results on HTML GUI
     '''
-    inputs = [x for x in request.form.values()]
-    result = " Hello There " + inputs[0]
-    return render_template('index.html', hello_text='{}'.format(result))
-
+    inputs = list(request.form.values())
+    result = f"Hello There {inputs[0]}"
+    return render_template('index.html', hello_text=result)
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0',port=8080)
+    APP.run(host='0.0.0.0', port=8080)
+    
